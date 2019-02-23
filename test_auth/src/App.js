66b27d23b1e4d718ce14c2cc8d10a85ed583145a
Marwan Helali <marwan01@Marwans-MacBook-Pro.local>
2019-02-23 17:24:09 -0600

    conditional rendering + states

--- test_auth/src/App.js
+++ test_auth/src/App.js
@@ -1,13 +1,31 @@
 // Import React
 import React, { Component } from 'react';
-
-import 'bootstrap/dist/css/bootstrap.css';
-
 import Home from './Home';
+import Welcome from './Welcome'
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 class App extends Component {
+
+constructor() {
+  super();
+  this.state = {
+    user: null
+  };
+}
+
   render() {
-    return <Home />;
+    return ( 
+      <div>
+        <Home user={this.state.user}/>
+        {this.state.user &&
+            <Welcome user={this.state.user}/>
+        }
+
+
+
+      </div>
+      );
   }
 }
 
--- test_auth/src/Home.js
+++ test_auth/src/Home.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
 
-class Home extends React.Component {
+class Home extends Component {
   render() {
+
+    const {user} = this.props;
     return (
       <div className="container text-center">
         <div className="row justify-content-center">
+        {user && (
+        <span>Welcome {user}</span>
+        )}
           <div className="col-10 col-md-10 col-lg-8 col-xl-7">
-            <div className="display-4 text-primary mt-3 mb-2">
-              Meeting Log
+            <div className="display-4 text-primary mt-3 mb-2"
+            style={{
+              fontSize:4 + 'em'
+              }}>
+              Athlytics
             </div>
             <p className="lead">
-              This simple app creates meetings, allows people to check
+              This simple app creates workouts, allows people to check
               in, and picks random users to award giveaways. It's a
               good example of a Single Page Application which includes
               connection to a database and routing. It's a practical
               way to learn <a href="https://reactjs.org/">React</a>
               with <a href="https://firebase.google.com">Firebase</a>.
             </p>
-
-            <a
+              {user == null && (
+                <span>
+                  <a
               href="/register"
               className="btn btn-outline-primary mr-2"
             >
@@ -27,9 +36,14 @@ class Home extends React.Component {
             <a href="/login" className="btn btn-outline-primary mr-2">
               Log In
             </a>
+                </span>
+              )}
+              {user && (
             <a href="/meetings" className="btn btn-primary">
-              Meetings
-            </a>
+            Workouts
+          </a>
+              )}
+
           </div>{' '}
           {/* columns */}
         </div>
