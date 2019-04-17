8796d68a70de8d57e7dda08fc8da90189a383a40
Ting <jting824@gmail.com>
2019-04-17 18:34:24 -0500

    updated package json

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Landing = () => (
   <div>
-    <h1>Landing</h1>
+
+
+      <h1> do somthing </h1>
+
+    <button>sign up</button>
+    <h1 >Landing</h1>
+    <h1>doiii</h1>
   </div>
 );
 
