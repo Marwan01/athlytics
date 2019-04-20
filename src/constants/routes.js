8128f84b8a519f8eab5a6ebd0f06531b45606c67
System Administrator <root@Marwans-MacBook-Pro.local>
2019-04-20 01:26:37 -0500

    Updates

--- src/constants/routes.js
+++
@@ -1,9 +0,0 @@
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
-export const HOME = '/';
-export const ACCOUNT = '/account';
-export const CALENDAR = '/calendar';
-export const WORKOUTS = '/workouts';
-export const PASSWORD_FORGET = '/pw-forget';
-export const ADMIN = '/admin';
-export const ADMIN_DETAILS = '/admin/:id';
