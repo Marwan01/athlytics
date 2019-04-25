a53e6fe304f197ba84aa3fe5cc9d72ed9bc81aeb
Mar <marwanest@live.fr>
2019-04-25 11:43:54 -0500

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -285,29 +285,3 @@ const HomepageLayout = () => (
   </ResponsiveContainer>
 )
 export default HomepageLayout
-
-/*class HomePage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Home Page</h1>
-        <p>The Home Page is accessible by every signed in user.</p>
-      </div>
-    );
-  }
-}
-
-const condition = authUser => !!authUser;
-
-export default compose(
-  withFirebase,
-  withEmailVerification,
-  withAuthorization(condition),
-)(HomePage);*/
