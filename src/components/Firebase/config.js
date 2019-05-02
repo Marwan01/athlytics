d0fc7d48fefcee8c2a9f113d7f50eb2fc2eb3f9c
Marwan Helali <marwan01@Marwans-MacBook-Pro.local>
2019-05-01 19:01:19 -0500

    got rid of config

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCGmvE7wqlcK4Qt36tFtAlb_WHFB_3S_nw",
+        authDomain: "athlytics5g.firebaseapp.com",
+        databaseURL: "https://athlytics5g.firebaseio.com",
+        projectId: "athlytics5g",
+        storageBucket: "athlytics5g.appspot.com",
+        messagingSenderId: "168458170038"
+};
+
+exports.config = config;
